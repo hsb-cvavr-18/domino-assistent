b5908b3d41c5e05a82fd27270e226241e2267349
czoeller <noex_home@yahoo.de>
2018-12-08 07:12:38 -0500

    Use proper window interaction

--- src/ImgDebugPrinter/DefaultImgDebugPrinter.cpp
+++ src/ImgDebugPrinter/DefaultImgDebugPrinter.cpp
@@ -5,11 +5,10 @@
 #include "ImgDebugPrinter.h"
 
 void DefaultImgDebugPrinter::preShowImage(std::string windowName) {
-    cv::namedWindow(windowName, true);
+    cv::namedWindow(windowName, cv::WINDOW_NORMAL);
 }
 
 void DefaultImgDebugPrinter::showImage(std::string windowName, int index, cv::Mat mat) {
-    cv::namedWindow(windowName, cv::WINDOW_NORMAL);
     cv::resizeWindow(windowName, mat.cols / 2, mat.rows / 2);
     cv::imshow(windowName, mat);
     /*
