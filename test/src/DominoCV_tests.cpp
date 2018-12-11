3c9d523cfbfdaa83b4912c5201bc18f16e5ed127
czoeller <noex_home@yahoo.de>
2018-12-11 11:49:49 -0500

    Ignore association of pips
    ,.(\d),.(\d) -> , {$1, $2}

--- test/src/DominoCV_tests.cpp
+++ test/src/DominoCV_tests.cpp
@@ -8,6 +8,7 @@
 
 #include <opencv2/core/mat.hpp>
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include <DominoCV.h>
 
 using namespace std;
@@ -24,7 +25,7 @@ protected:
     virtual void TearDown() {
     };
 
-    virtual void verify(string previousImg, string currentImg, int pipsAExpected, int pipsBExpected) {
+    virtual void verify(string previousImg, string currentImg, const std::initializer_list<int>& pipsExpected) {
 
         cv::Mat previousImgMat = cv::imread(previousImg);
         if (!previousImgMat.data) {
@@ -38,122 +39,122 @@ protected:
 
         const dominoPiece &dominoPiece = detectPiece(previousImgMat, currentImgMat);
 
-        EXPECT_EQ(dominoPiece.a.pips, pipsAExpected);
-        EXPECT_EQ(dominoPiece.b.pips, pipsBExpected);
+        const unsigned int actualPips[2] = {(dominoPiece.a.pips), (dominoPiece.b.pips)};
+        ASSERT_THAT(actualPips, testing::UnorderedElementsAreArray(pipsExpected));
     }
 };
 
 TEST_F(DominoCVTest, gestell0To1) {
-    verify("../gestell_0.jpg", "../gestell_1.jpg", 2, 6);
+    verify("../gestell_0.jpg", "../gestell_1.jpg", {2, 6});
 }
 
 TEST_F(DominoCVTest, gestell6To7) {
-    verify("../gestell_6.jpg", "../gestell_7.jpg", 1, 5);
+    verify("../gestell_6.jpg", "../gestell_7.jpg", {1, 5});
 }
 
 TEST_F(DominoCVTest, gestell7To8) {
-    verify("../gestell_7.jpg", "../gestell_8.jpg", 6, 1);
+    verify("../gestell_7.jpg", "../gestell_8.jpg", {6, 1});
 }
 
 TEST_F(DominoCVTest, gestell8To9) {
-    verify("../gestell_8.jpg", "../gestell_9.jpg", 6, 6);
+    verify("../gestell_8.jpg", "../gestell_9.jpg", {6, 6});
 }
 
 TEST_F(DominoCVTest, gestell9To10) {
-    verify("../gestell_9.jpg", "../gestell_10.jpg", 0, 6);
+    verify("../gestell_9.jpg", "../gestell_10.jpg", {0, 6});
 }
 
 TEST_F(DominoCVTest, gestell10To11) {
-    verify("../gestell_10.jpg", "../gestell_11.jpg", 1, 2);
+    verify("../gestell_10.jpg", "../gestell_11.jpg", {1, 2});
 }
 
 TEST_F(DominoCVTest, gestell11To12) {
-    verify("../gestell_11.jpg", "../gestell_12.jpg", 1, 1);
+    verify("../gestell_11.jpg", "../gestell_12.jpg", {1, 1});
 }
 
 TEST_F(DominoCVTest, gestell12To13) {
-    verify("../gestell_12.jpg", "../gestell_13.jpg", 1, 3);
+    verify("../gestell_12.jpg", "../gestell_13.jpg", {1, 3});
 }
 
 /**
  * GESTELL 2
  */
 TEST_F(DominoCVTest, gestell2_0To1) {
-    verify("../gestell2_0.jpg", "../gestell2_1.jpg", 2, 4);
+    verify("../gestell2_0.jpg", "../gestell2_1.jpg", {2, 4});
 }
 
 TEST_F(DominoCVTest, gestell2_1To2) {
-    verify("../gestell2_1.jpg", "../gestell2_2.jpg", 2, 0);
+    verify("../gestell2_1.jpg", "../gestell2_2.jpg", {2, 0});
 }
 
 TEST_F(DominoCVTest, gestell2_2To3) {
-    verify("../gestell2_2.jpg", "../gestell2_3.jpg", 6, 0);
+    verify("../gestell2_2.jpg", "../gestell2_3.jpg", {6, 0});
 }
 
 TEST_F(DominoCVTest, gestell2_3To4) {
-    verify("../gestell2_3.jpg", "../gestell2_4.jpg", 1, 4);
+    verify("../gestell2_3.jpg", "../gestell2_4.jpg", {1, 4});
 }
 
 TEST_F(DominoCVTest, gestell2_4To5) {
-    verify("../gestell2_4.jpg", "../gestell2_5.jpg", 2, 1);
+    verify("../gestell2_4.jpg", "../gestell2_5.jpg", {2, 1});
 }
 
 TEST_F(DominoCVTest, gestell2_5To6) {
-    verify("../gestell2_5.jpg", "../gestell2_6.jpg", 3, 2);
+    verify("../gestell2_5.jpg", "../gestell2_6.jpg", {3, 2});
 }
 
 TEST_F(DominoCVTest, gestell2_6To7) {
-    verify("../gestell2_6.jpg", "../gestell2_7.jpg", 3, 3);
+    verify("../gestell2_6.jpg", "../gestell2_7.jpg", {3, 3});
 }
 
 TEST_F(DominoCVTest, gestell2_7To8) {
     // Autofokus
-    // verify("../gestell2_7.jpg", "../gestell2_8.jpg", 6, 6);
+    // verify("../gestell2_7.jpg", "../gestell2_8.jpg", {6, 6});
 }
 
 TEST_F(DominoCVTest, gestell2_8To9) {
     // Autofokus
-    // verify("../gestell2_8.jpg", "../gestell2_9.jpg", 6, 5);
+    // verify("../gestell2_8.jpg", "../gestell2_9.jpg", {6, 5});
 }
 
 TEST_F(DominoCVTest, gestell2_9To10) {
-    verify("../gestell2_9.jpg", "../gestell2_10.jpg", 3, 5);
+    verify("../gestell2_9.jpg", "../gestell2_10.jpg", {3, 5});
 }
 
 TEST_F(DominoCVTest, gestell2_10To11) {
-    verify("../gestell2_10.jpg", "../gestell2_11.jpg", 5, 2);
+    verify("../gestell2_10.jpg", "../gestell2_11.jpg", {5, 2});
 }
 
 TEST_F(DominoCVTest, gestell2_11To12) {
-    verify("../gestell2_11.jpg", "../gestell2_12.jpg", 0, 5);
+    verify("../gestell2_11.jpg", "../gestell2_12.jpg", {0, 5});
 }
 
 TEST_F(DominoCVTest, gestell2_12To13) {
-    verify("../gestell2_12.jpg", "../gestell2_13.jpg", 1, 0);
+    verify("../gestell2_12.jpg", "../gestell2_13.jpg", {1, 0});
 }
 
 TEST_F(DominoCVTest, gestell2_13To14) {
-    verify("../gestell2_13.jpg", "../gestell2_14.jpg", 6, 1);
+    verify("../gestell2_13.jpg", "../gestell2_14.jpg", {6, 1});
 }
 
 TEST_F(DominoCVTest, gestell2_14To15) {
-    verify("../gestell2_14.jpg", "../gestell2_15.jpg", 3, 6);
+    verify("../gestell2_14.jpg", "../gestell2_15.jpg", {3, 6});
 }
 
 TEST_F(DominoCVTest, gestell2_15To16) {
-    verify("../gestell2_15.jpg", "../gestell2_16.jpg", 4, 3);
+    verify("../gestell2_15.jpg", "../gestell2_16.jpg", {4, 3});
 }
 
 TEST_F(DominoCVTest, gestell2_16To17) {
-    verify("../gestell2_16.jpg", "../gestell2_17.jpg", 4, 4);
+    verify("../gestell2_16.jpg", "../gestell2_17.jpg", {4, 4});
 }
 
 TEST_F(DominoCVTest, gestell2_17To18) {
-    verify("../gestell2_17.jpg", "../gestell2_18.jpg", 6, 4);
+    verify("../gestell2_17.jpg", "../gestell2_18.jpg", {6, 4});
 }
 
 TEST_F(DominoCVTest, gestell2_18To19) {
-    verify("../gestell2_18.jpg", "../gestell2_19.jpg", 2, 6);
+    verify("../gestell2_18.jpg", "../gestell2_19.jpg", {2, 6});
 }
 
 /**
@@ -161,19 +162,19 @@ TEST_F(DominoCVTest, gestell2_18To19) {
  */
 
 TEST_F(DominoCVTest, gestell3_0To1) {
-    verify("../gestell3_0.jpg", "../gestell3_1.jpg", 1, 6);
+    verify("../gestell3_0.jpg", "../gestell3_1.jpg", {1, 6});
 }
  
 TEST_F(DominoCVTest, gestell3_1To2) {
-    verify("../gestell3_1.jpg", "../gestell3_2.jpg", 1, 5);
+    verify("../gestell3_1.jpg", "../gestell3_2.jpg", {1, 5});
 }
 
 TEST_F(DominoCVTest, gestell3_2To3) {
-    verify("../gestell3_2.jpg", "../gestell3_3.jpg", 5, 0);
+    verify("../gestell3_2.jpg", "../gestell3_3.jpg", {5, 0});
 }
 
 TEST_F(DominoCVTest, gestell3_3To4) {
-    verify("../gestell3_3.jpg", "../gestell3_4.jpg", 1, 0);
+    verify("../gestell3_3.jpg", "../gestell3_4.jpg", {1, 0});
 }
 
 /**
@@ -181,9 +182,9 @@ TEST_F(DominoCVTest, gestell3_3To4) {
  */
 
 TEST_F(DominoCVTest, gestell4_2To3) {
-    verify("../gestell4_2.jpg", "../gestell4_3.jpg", 5, 0);
+    verify("../gestell4_2.jpg", "../gestell4_3.jpg", {5, 0});
 }
 
 TEST_F(DominoCVTest, gestell4_3To4) {
-    verify("../gestell4_3.jpg", "../gestell4_4.jpg", 1, 0);
+    verify("../gestell4_3.jpg", "../gestell4_4.jpg", {1, 0});
 }
