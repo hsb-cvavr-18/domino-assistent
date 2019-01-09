8b5f26480456a074a60283eff066459e01adf034
Markus <klugem89@web.de>
2019-01-09 11:41:50 -0500

    add special case for paschStone

--- src/Game/DominoPiece.cpp
+++ src/Game/DominoPiece.cpp
@@ -34,10 +34,17 @@ cv::Point2f DominoPiece::getCenter() {
 }
 
 bool DominoPiece::isApplicableTo(DominoPiece piece) {
-    return this->getHalfA().isApplicableTo(piece.getHalfA()) ||
-           this->getHalfA().isApplicableTo(piece.getHalfB()) ||
-           this->getHalfB().isApplicableTo(piece.getHalfA()) ||
-           this->getHalfB().isApplicableTo(piece.getHalfB());
+    if(this->isNormalStone()) {
+        return this->getHalfA().isApplicableTo(piece.getHalfA()) ||
+               this->getHalfA().isApplicableTo(piece.getHalfB()) ||
+               this->getHalfB().isApplicableTo(piece.getHalfA()) ||
+               this->getHalfB().isApplicableTo(piece.getHalfB());
+    }else{
+        this->getHalfA().getNumber()==piece.getHalfA().getNumber() ||
+        this->getHalfA().getNumber()==piece.getHalfB().getNumber() ||
+        this->getHalfB().getNumber()==piece.getHalfA().getNumber() ||
+        this->getHalfB().getNumber()==piece.getHalfB().getNumber() ||
+    }
 }
 
 void DominoPiece::block(int num) {
